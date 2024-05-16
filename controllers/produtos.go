82a5b0f12b0a64a161443790fe22bad9573b7d0a
Maria <malufln.99@gmail.com>
2024-05-16 12:25:44 -0300

    Tela de edição de produto

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -46,3 +46,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	models.DeletaProduto(idProduto)
 	http.Redirect(w, r, "/", 301)
 }
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Edit", nil)
+}
